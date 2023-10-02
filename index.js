5826a0706b935bd1ca76f5dcf6e321b27c174b84
tanushka-jha23 <tanushkajha670@gmail.com>
2023-10-02 18:22:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu-container")
+const close = document.querySelector(".cross")
+const view = document.querySelector(".view-button")
+const invisible = document.querySelector(".invisible")
+const addCart = document.querySelector(".buy")
+const like = document.querySelectorAll(".liked")
+const drop = () => {
+    menu.style.transform = "translateY(-12vh)"
+    menu.style.zIndex = "1"
+}
+hamburger.addEventListener("click", drop)
+close.addEventListener("click", () => {
+    menu.style.transform = "translateY(-120vh)"
+})
+view.addEventListener("click", () => {
+    invisible.className = "product-container"
+    view.textContent = "View Less"
+})
+like.addEventListener("click", () => {
+    like.style.color = "red"
+})
